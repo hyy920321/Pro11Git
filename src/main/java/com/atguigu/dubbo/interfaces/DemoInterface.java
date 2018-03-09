8ce66a2cfeebe84291b223b07c272e082893467b
yunyao <yunyao@atguigu.com>
2018-03-09 20:46:28 +0800

    make conflict

--- src/main/java/com/atguigu/dubbo/interfaces/DemoInterface.java
+++ src/main/java/com/atguigu/dubbo/interfaces/DemoInterface.java
@@ -5,6 +5,7 @@ public interface DemoInterface {
 	String getMessageRemote(String param);
 	String getMessageRemote1(String param);
 	String getMessageRemote2(String param);
+	String getMessageRemote4(String param);
 	
 	
 
