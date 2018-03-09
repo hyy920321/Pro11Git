f5177eb1cec2124a9d315ef095bcf007ccc26bab
yunyao <yunyao@atguigu.com>
2018-03-09 20:35:16 +0800

    second

--- src/main/java/com/atguigu/dubbo/interfaces/DemoInterface.java
+++ src/main/java/com/atguigu/dubbo/interfaces/DemoInterface.java
@@ -3,5 +3,9 @@ package com.atguigu.dubbo.interfaces;
 public interface DemoInterface {
 	
 	String getMessageRemote(String param);
+	String getMessageRemote1(String param);
+	String getMessageRemote2(String param);
+	
+	
 
 }
